9ca86ed51ad0dfc8dc239c4237087d2016aa35e7
Anil Bhanushali <anil@better-life.co.in>
2018-07-21 23:11:15 +0530

    add docs

--- index.js
+++ index.js
@@ -13,20 +13,8 @@ app.post('/transaction', (req, res) => {
         .catch(error => res.status(500).send(error));
 })
 app.get('/', (req, res) => {
-    res.send(`
-    <div class="postman-run-button"
-    data-postman-action="collection/import"
-    data-postman-var-1="a4a336aa97ebdc262a49"></div>
-    <script type="text/javascript">
-        (function (p,o,s,t,m,a,n) {
-            !p[s] && (p[s] = function () { (p[t] || (p[t] = [])).push(arguments); });
-            !o.getElementById(s+t) && o.getElementsByTagName("head")[0].appendChild((
-            (n = o.createElement("script")),
-            (n.id = s+t), (n.async = 1), (n.src = m), n
-            ));
-        }(window, document, "_pm", "PostmanRunObject", "https://run.pstmn.io/button.js"));
-    </script>
-    `)
+    res.redirect('https://documenter.getpostman.com/view/1002001/RWMHKSKF');
+    
 })
 app.get('/balance', (req, res) => {
     bank.getBalance({
